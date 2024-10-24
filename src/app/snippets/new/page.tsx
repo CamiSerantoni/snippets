d876b2d48db365ceace0b8ae6285efaf11efcc90
Camila Serantoni <camila.serantoni@blue.cl>
2024-10-23 22:40:39 -0300

    adding a creation form

--- src/app/snippets/new/page.tsx
+++ src/app/snippets/new/page.tsx
@@ -1,7 +1,37 @@
-export default function SnippetsCreatePage(){
-    return (
-        <div>
-           Create Snippets!
+export default function SnippetsCreatePage() {
+  return (
+    <form action="">
+      <h3 className="font-bold m-3">Create snippet</h3>
+      <div className="flex  flex-col gap-4">
+        <div className="flex gap-4">
+          <label htmlFor="title" className="w-12">
+            {" "}
+            Title
+          </label>
+          <input
+            type="text"
+            name="title"
+            className="border rounded p-2 w-full"
+          />
         </div>
-    )
-} 
+
+        <div className="flex  flex-col gap-4">
+          <div className="flex gap-4">
+            <label htmlFor="code" className="w-12">
+              {" "}
+              Code
+            </label>
+            <textarea
+              name="code"
+              className="border rounded p-2 w-full"
+              id="code"
+            />
+          </div>
+          <button type="submit" className="rounded p-2 bg-blue-200">
+            Create
+          </button>
+        </div>
+      </div>
+    </form>
+  );
+}
