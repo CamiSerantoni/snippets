daa51a3a7f88755f5928338a883aa807ed038483
Camila Serantoni <camila.serantoni@blue.cl>
2024-10-25 22:15:17 -0300

    fetching particular records

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,8 +1,26 @@
-export default function SnippetsShowPage(props: any) {
-console.log(props)
+
+import { notFound } from 'next/navigation'
+import { db } from '@/db'
+
+interface SnippetsShowPageProps {
+    params: {
+        id:string
+    }
+}
+
+export default async  function SnippetsShowPage(props: SnippetsShowPageProps) {
+
+const snippet = await db.snippet.findFirst({
+    where: {id: parseInt(props.params.id)}
+})
+
+if(!snippet) {
+    return notFound()
+}
+
     return (
         <div>
-        Show a snippet
+        {snippet.title}
         </div>
     );
 }
