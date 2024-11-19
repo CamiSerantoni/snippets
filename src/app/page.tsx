495dcda27cceabf839e8b72c49a83e008314c630
Camila Serantoni <camila.serantoni@blue.cl>
2024-11-18 23:12:56 -0300

    styling show page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,15 +7,17 @@ export default async function Home() {
 
   const renderedSnippets = snippets.map((snippet) => {
     return (
-      <Link href={`/snippets/${snippet.id}`}  key={snippet.id} className="flex justify-between items-center p-2 border rounded ">
+
+      
+      <Link href={`/snippets/${snippet.id}`} key={snippet.id} className="flex justify-between items-center p-2 border rounded ">
         {snippet.title}
         {snippet.code}
-     
-     <div>
-      {snippet.title}
-     </div>
-     <div>View</div>
-     
+
+        <div>
+          {snippet.title}
+        </div>
+        <div>View</div>
+
       </Link>
     );
   });
@@ -26,12 +28,12 @@ export default async function Home() {
         <h1 className="text-xl font-bold">Snippets</h1>
         <Link href={"/snippets/new"} className="border p-2 rounded">New</Link>
       </div>
-<div className="flex flex-col gap-2">
-  {renderedSnippets}
-</div>
+      <div className="flex flex-col gap-2">
+        {renderedSnippets}
+      </div>
 
-      
-    
+
+
     </div>
   );
 }
