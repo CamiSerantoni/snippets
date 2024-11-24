c68d23cc14115d5a7343b51c11b2455d285a1a30
Camila Serantoni <camila.serantoni@blue.cl>
2024-11-23 21:40:19 -0300

    adding monaco editor

--- src/components/snippet-edit-form.tsx
+++ src/components/snippet-edit-form.tsx
@@ -1,12 +1,14 @@
 'use client'
 import type { Snippet } from '@prisma/client'
-
+import Editor from '@monaco-editor/react'
 interface SnippetEditFormProps {
     snippet: Snippet
 }   
 export default function SnippetEditForm({snippet}: SnippetEditFormProps) {
     return (
-<div>Client component has snippet with title:::: {snippet.title}    </div>
+<div>
+    <Editor height="40vh" theme='vs-dark' language='javascript' defaultValue={snippet.code} options={{minimap: {enabled: false}}} />
+      </div>
 
     )
 }        
